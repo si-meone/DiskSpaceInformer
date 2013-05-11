3530d23c23dd07ad79a915176a32baa50479f9ed
Nasrallah <simon_nasrallah@hotmail.com>
2013-05-11 23:30:52 +0100

    new jar

--- src/DiskSpaceInformer.java
+++ src/DiskSpaceInformer.java
@@ -23,7 +23,7 @@ public class DiskSpaceInformer extends JPanel
     protected FindFileAndFolderSizes task;
     protected JProgressBar progressBar;
 
-    private static String version = "Disk Space Informer v0.1h";
+    private static String version = "Disk Space Informer v0.1i";
     static private final String newline = "\n";
     private final JComboBox drives;
 
