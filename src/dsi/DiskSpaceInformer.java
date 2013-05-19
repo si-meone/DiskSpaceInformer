f128180cb608cf7d96f7443026d9d26f34d95407
Nasrallah <simon_nasrallah@hotmail.com>
2013-05-19 13:55:38 +0100

    cranked version up to show new ant built

--- src/dsi/DiskSpaceInformer.java
+++ src/dsi/DiskSpaceInformer.java
@@ -27,7 +27,7 @@ public class DiskSpaceInformer extends JPanel
     protected FindFileAndFolderSizes task;
     protected JProgressBar progressBar;
 
-    private static String version = "Disk Space Informer v0.1n";
+    private static String version = "Disk Space Informer v0.1p";
     static private final String newline = "\n";
     private final JComboBox drives;
 
